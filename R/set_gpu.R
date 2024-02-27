296b365ab8ef0b34c723f1551cea80352adc6f11
vjcitn <stvjc@channing.harvard.edu>
2024-02-27 12:50:24 -0500

    new config functions

---
+++ R/set_gpu.R
@@ -0,0 +1,18 @@
+#' needed to ensure code for gpu usage is compiled
+#' @note Runs basilisk-managed pip to compile and install
+#' GPU-enabled torchbiggraph.
+#' @export
+set_gpu = function() {
+pippath = (file.path(basilisk.utils::getExternalDir(), "BiocBigGraph", "0.0.3", "bsklenv", "bin", "pip"))
+tpath = system.file("python", "fbsource", package="BiocBigGraph")
+system2(pippath, c("install", tpath), env=c("PBG_INSTALL_CPP=1"))
+}
+
+#' needed to allow subprocess to find torchbiggraph for train module
+#' @note Runs basilisk-managed pip to compile and install
+#' @export
+set_train = function() {
+pippath = (file.path(basilisk.utils::getExternalDir(), "BiocBigGraph", "0.0.3", "bsklenv", "bin", "pip"))
+tpath = system.file("python", "fbsource", package="BiocBigGraph")
+system2(pippath, c("install", tpath))
+}
---
+++ R/zzz.R
@@ -0,0 +1,5 @@
+#.onLoad = function(libname, pkgname) {
+#pippath = (file.path(basilisk.utils::getExternalDir(), "BiocBigGraph", "0.0.3", "bsklenv", "bin", "pip"))
+#tpath = system.file("python", "fbsource", package="BiocBigGraph")
+#system2(pippath, c("install", tpath), env=c("PBG_INSTALL_CPP=1"))
+#}
