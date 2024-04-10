171712a9c016e2f0be28866c0370cd3ecd5ab29b
vjcitn <stvjc@channing.harvard.edu>
2024-04-10 16:08:19 -0400

    moving forward with ingest example

--- R/ingest_embeddings.R
+++ R/ingest_embeddings.R
@@ -1,6 +1,17 @@
 #' work with the output of CG_embed_sce
 #' @param outemb list derived from configuration schema produced
-#' in the embedding function
+#' in the embedding function; for demo4, use `make_demo_emb`
+#' @examples
+#' example(make_demo_emb) # creates mm
+#' nnn = ingest_embeddings(mm)
+#' names(nnn)
+#' pp = prcomp(t(nnn$cemb))
+#' data(p3k.fine)
+#' p3k=TENxPBMCData("pbmc3k")
+#' p3k = scuttle::logNormCounts(p3k)
+#' mm = match(p3k$Barcode, nnn$C_entities_ordered)
+#' pairs(pp$x[mm,2:4], pch=19, cex=.4, col=factor(p3k.fine))
+#' @export
 ingest_embeddings = function(outemb) {
 #
 # define a class for the embed_sce run?
