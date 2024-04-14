72a076cbe46bde32e0202f2da1ba295876b66a37
vjcitn <stvjc@channing.harvard.edu>
2024-04-14 14:00:45 -0400

    add some help for sce retrieval and simba scores

---
+++ R/prep_simba.R
@@ -0,0 +1,49 @@
+#' update an SCE with simba edge scores
+#' @param embs output of sce_to_embeddings
+#' @param sce the associated sce
+#' @return SCE updated with altExp giving the simba scores
+#' @examples
+#' data(nn50)
+#' data(t3k)
+#' t3k = add_simba_scores(nn50, t3k)
+#' t3k
+#' @export
+add_simba_scores = function(embs, sce) {
+  ep = embs_to_scores(embs, sce)
+  altExp(sce, "sprobs", withDimnames=FALSE) = ep
+  sce
+}
+
+#' set up a data.frame for barplot visualization for a gene
+#' @param sce SingleCellExperiment with altExp element "sprobs"
+#' @param genesym character(1)
+#' @param colvars character() columns from colData(sce) to propagate
+#' @return data.frame with elements rank, prob, and propagated columns
+#' @examples
+#' data(nn50)
+#' data(t3k)
+#' t3k = add_simba_scores(nn50, t3k)
+#' gns = c("CST3", "MS4A1", "NKG7", "GAPDH")
+#' alldf = lapply(gns, function(x) simba_barplot_df(t3k, x, 
+#'   colvars = "celltype"))
+#' alldf = do.call(rbind, alldf)
+#' ggplot(alldf, aes(x=rank,xend=rank,y=0, yend=prob,colour=celltype)) + geom_point() +
+#'   geom_segment() + facet_wrap(~symbol, ncol=2) + 
+#'     theme(strip.text=element_text(size=24), 
+#'       legend.text=element_text(size=24), 
+#'       axis.text=element_text(size=24)) +
+#'     guides(colour = guide_legend(override.aes = 
+#'        list(linetype=1, lwd=2)))
+#' @export
+simba_barplot_df = function(sce, genesym, colvars="celltype") {
+  stopifnot("sprobs" %in% altExpNames(sce))
+  stopifnot(genesym %in% rownames(altExp(sce, "sprobs")))
+  stopifnot(all(colvars %in% names(colData(sce))))
+  resp = as.numeric(assay(altExp(sce, "sprobs"))[genesym,])
+  o = order(resp, decreasing=TRUE)
+  ans = data.frame(rank=seq_len(length(o)), prob = resp[o], tmp=colData(sce)[o,colvars])
+  names(ans)[-c(1,2)] = colvars
+  ans$symbol = genesym
+  ans
+}
+
